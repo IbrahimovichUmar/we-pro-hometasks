b089e8d9f67dd341f24d1f685f5e11d4d61dff8f
Umar <umaribragimovich555@gmail.com>
2022-11-23 23:27:28 +0500

    feat: the homework of 22 nd November has done

---
+++ index.js
@@ -0,0 +1,41 @@
+// Complete class task
+
+// let name = prompt("What is your name?").toUpperCase().trim();
+// let age = confirm("Are you upper 18?");
+
+// if (name[0] === "A" && age === true) {
+//   console.log("Welcome");
+// } else {
+//   console.log("Go home");
+// }
+
+// Even or odd
+
+// let int = prompt("any number");
+
+// if (int % 2 == 0) {
+//   console.log("число четное");
+// } else {
+//   console.log("число нечетное");
+// }
+
+// Quantity of numbers should be even
+
+// let userName = prompt("What is your name?");
+// let userAge = prompt("How old are you?");
+
+// if (userName.length % 2 == 0 && userAge >= 18) {
+//   console.log("Welcome");
+// } else {
+//   console.log("Go home");
+// }
+
+// Presidents include
+
+// let guess = prompt("Mirziyoyev Putin Obama Biden Zelensky Trump");
+
+// if (guess.includes()) {
+//   console.log("You are president");
+// } else {
+//   console.log("You are not president");
+// }
