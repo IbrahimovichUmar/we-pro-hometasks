dd211171ea0e3e56084e05cf0e866aecb1e89e4a
Umar <umaribragimovich555@gmail.com>
2022-12-03 12:49:46 +0500

    3rd of December hometasks completed

--- index.js
+++ index.js
@@ -68,27 +68,199 @@
 //   console.log("Go home");
 // }
 
-let nameOfUser = prompt("What is your name?").toUpperCase();
+// let nameOfUser = prompt("What is your name?").toUpperCase();
 
-if (nameOfUser[0] === "A") {
-  let ageOfUser = prompt("How old are you?");
-  if (ageOfUser > 20 && ageOfUser <= 40) {
-    let moneyOfUser = prompt("How much money do you have?");
-    if (moneyOfUser > 100) {
-      let sizeOfUser = prompt("How many are you?");
-      let clubOccupied = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-      let freePlace = sizeOfUser - clubOccupied;
-      if (sizeOfUser <= 10) {
-        console.log("Come on");
-      } else {
-        console.log(`There is only ${freePlace} free places`);
-      }
-    } else {
-      console.log("Not enough");
-    }
+// if (nameOfUser[0] === "A") {
+//   let ageOfUser = prompt("How old are you?");
+//   if (ageOfUser > 20 && ageOfUser <= 40) {
+//     let moneyOfUser = prompt("How much money do you have?");
+//     if (moneyOfUser > 100) {
+//       let sizeOfUser = prompt("How many are you?");
+//       if (sizeOfUser <= 10) {
+//         console.log("Come on");
+//       } else {
+//         console.log("Too many");
+//       }
+//     } else {
+//       console.log("Not enough");
+//     }
+//   } else {
+//     console.log("Wrong age");
+//   }
+// } else {
+//   console.log("Wrong name");
+// }
+
+// console.log(numbers.sort((a, b) => a - b));
+
+// Task 1
+let students = [
+  {
+    name: "Timur",
+    age: 17,
+    gender: "male",
+  },
+  {
+    name: "Zafar",
+    age: 17,
+    gender: "male",
+  },
+  {
+    name: "Nigina",
+    age: 17,
+    gender: "female",
+  },
+  {
+    name: "Madina",
+    age: 16,
+    gender: "female",
+  },
+  {
+    name: "Samir",
+    age: 14,
+    gender: "male",
+  },
+  {
+    name: "Aziz",
+    age: 15,
+    gender: "male",
+  },
+  {
+    name: "Mirjalol",
+    age: 18,
+    gender: "male",
+  },
+  {
+    name: "Umar",
+    age: 16,
+    gender: "male",
+  },
+  {
+    name: "Maxdi",
+    age: 15,
+    gender: "male",
+  },
+  {
+    name: "Munis",
+    age: 18,
+    gender: "male",
+  },
+  {
+    name: "Laziz",
+    age: 15,
+    gender: "male",
+  },
+];
+
+let total = students.reduce((a, b) => {
+  return a + b.age;
+}, 0);
+
+let average = total / students.length;
+
+console.log(average.toFixed(1));
+
+let male = [];
+let female = [];
+
+students.filter((student) => {
+  if (student.gender === "male") {
+    male.push(student);
   } else {
-    console.log("Wrong age");
+    female.push(student);
   }
-} else {
-  console.log("Wrong name");
-}
+});
+
+console.log(male, female);
+
+// Task 2
+
+let products = [
+  {
+    userId: 1,
+    id: 1,
+    title: "delectus aut au`tem",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 2,
+    title: "quis ut nam facilis et officia qui",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 3,
+    title: "fugiat veniam minus",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 4,
+    title: "et porro tempora",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 5,
+    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 6,
+    title: "qui ullam ratione quibusdam voluptatem quia omnis",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 7,
+    title: "illo expedita consequatur quia in",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 8,
+    title: "quo adipisci enim quam ut ab",
+    completed: true,
+  },
+  {
+    userId: 1,
+    id: 9,
+    title: "molestiae perspiciatis ipsa",
+    completed: false,
+  },
+  {
+    userId: 1,
+    id: 10,
+    title: "illo est ratione doloremque quia maiores aut",
+    completed: true,
+  },
+];
+let a = {
+  count: 0,
+  arr: [],
+};
+
+let b = {
+  count: 0,
+  arr: [],
+};
+
+products.filter((product) => {
+  if (product.completed === true) {
+    a.arr.push(product);
+  } else {
+    b.arr.push(product);
+  }
+});
+
+products.filter((number) => {
+  if (number.completed === true) {
+    a.count++;
+  } else {
+    b.count++;
+  }
+});
+
+console.log(a, b);
