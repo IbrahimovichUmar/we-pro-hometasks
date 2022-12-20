a608599fd7ed30262152a282a60f39afe324be85
Umar <umaribragimovich555@gmail.com>
2022-12-20 12:23:36 +0500

    feat: 20th of December hometask

--- script.js
+++ script.js
@@ -1,94 +1,153 @@
-// let n = +prompt();
-
-// for (let i = 1; i <= 10; i++) {
-//   document.write(`${n} * ${i} = ${n * i} </br>`);
-// }
-// console.log([1, 2, 3, 4].filter((item) => item % 2 === 0));
-
-let arr = [
+let products = [
   {
-    userId: 1,
-    id: 1,
-    title: "delectus aut au`tem",
-    completed: false,
+    title: "moloko",
+    type: "milk",
+    price: 10000,
   },
   {
-    userId: 1,
-    id: 2,
-    title: "quis ut nam facilis et officia qui",
-    completed: false,
+    title: "potato",
+    type: "vegetables",
+    price: 6000,
   },
   {
-    userId: 1,
-    id: 3,
-    title: "fugiat veniam minus",
-    completed: false,
+    title: "chocolate",
+    type: "milk",
+    price: 18000,
   },
   {
-    userId: 1,
-    id: 4,
-    title: "et porro tempora",
-    completed: true,
+    title: "cheese",
+    type: "milk",
+    price: 64000,
   },
   {
-    userId: 1,
-    id: 5,
-    title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    completed: false,
+    title: "pivo",
+    type: "alcohol",
+    price: 12000,
   },
   {
-    userId: 1,
-    id: 6,
-    title: "qui ullam ratione quibusdam voluptatem quia omnis",
-    completed: false,
+    title: "tomato",
+    type: "vegetables",
+    price: 17000,
   },
   {
-    userId: 1,
-    id: 7,
-    title: "illo expedita consequatur quia in",
-    completed: false,
+    title: "zelen",
+    type: "vegetables",
+    price: 2500,
   },
   {
-    userId: 1,
-    id: 8,
-    title: "quo adipisci enim quam ut ab",
-    completed: true,
+    title: "yogurt",
+    type: "milk",
+    price: 11000,
   },
   {
-    userId: 1,
-
-    id: 9,
-    title: "molestiae perspiciatis ipsa",
-    completed: false,
+    title: "viski",
+    type: "alcohol",
+    price: 540000,
   },
   {
-    userId: 1,
-    id: 10,
-    title: "illo est ratione doloremque quia maiores aut",
-    completed: true,
+    title: "champane",
+    type: "alcohol",
+    price: 70000,
+  },
+  {
+    title: "moloko",
+    type: "milk",
+    price: 10000,
+  },
+  {
+    title: "potato",
+    type: "vegetables",
+    price: 6000,
+  },
+  {
+    title: "chocolate",
+    type: "milk",
+    price: 18000,
+  },
+  {
+    title: "cheese",
+    type: "milk",
+    price: 64000,
+  },
+  {
+    title: "pivo",
+    type: "alcohol",
+    price: 12000,
+  },
+  {
+    title: "tomato",
+    type: "vegetables",
+    price: 17000,
+  },
+  {
+    title: "zelen",
+    type: "vegetables",
+    price: 2500,
+  },
+  {
+    title: "yogurt",
+    type: "milk",
+    price: 11000,
+  },
+  {
+    title: "viski",
+    type: "alcohol",
+    price: 540000,
+  },
+  {
+    title: "champane",
+    type: "alcohol",
+    price: 70000,
   },
 ];
-let a = {
-  count: 0,
-  arr: [],
-};
 
-let b = {
-  count: 0,
-  arr: [],
-};
+let categories = [
+  {
+    type: "vegetables",
+    count: 0,
+    total: 0,
+  },
+  {
+    type: "alcohol",
+    count: 0,
+    total: 0,
+  },
+  {
+    type: "milk",
+    count: 0,
+    total: 0,
+  },
+];
 
-for (let i = 0; i <= arr.length - 1; i++) {
-  if (arr[i].completed === true) {
-    a.arr.push(arr[i]);
-    a.count++;
-  } else {
-    b.arr.push(arr[i]);
-    b.count++;
+// Создать категориям новый ключ arr
+// В эти ключи запушить продукты относящиеся этой же категории
+// Сортировать ключ arr по алфавиту
+
+// for (let item of products) {
+//   for (let itemTwo of catefories) {
+//     if (item.type === itemTwo.type) {
+//       itemTwo.count++;
+//       itemTwo.total += item.price;
+//     }
+//   }
+// }
+
+// console.log(catefories);
+
+// for (let item of products) {
+//   console.log(item.price);
+// }
+
+for (let secondItem of categories) {
+  secondItem.arr = [];
+  for (item of products) {
+    if (item.type === "vegetables") {
+      secondItem.arr.push(item);
+    } else if (item.type === "alcohol") {
+      secondItem.arr.push(item);
+    } else {
+      secondItem.arr.push(item);
+    }
   }
 }
-console.log(a, b);
-
-// for (let item of arr) {
-//   console.log(item);
-// }
+console.log(categories);
