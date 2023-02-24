7621cd6a28b20b2dd4a67dd17e033566f5a246a5
Umar <umaribragimovich555@gmail.com>
2023-02-24 14:30:56 +0500

    feat: Fubruary 24

--- other/index.js
+++ other/index.js
@@ -264,3 +264,334 @@
 // });
 
 // console.log(a, b);
+
+// let courses = [
+//     {
+//       title: "SMM",
+
+//       howLong: 3,
+//     },
+
+//     {
+//       title: "Dev",
+
+//       howLong: 6,
+//     },
+
+//     {
+//       title: "Graph",
+
+//       howLong: 4,
+//     },
+//   ];
+
+//   let earnings = {
+//     total: 0,
+
+//     max: 0,
+
+//     min: 0,
+//   };
+
+//   let m = Number;
+
+//   let w = Number;
+
+//   let average_age = Number;
+
+//   let smm = Number;
+
+//   let dev = Number;
+
+//   let graph = Number;
+
+//   let students = [
+//     {
+//       sex: "m",
+
+//       age: 14,
+
+//       payment: 1220000,
+
+//       course: {
+//         title: "SMM",
+//       },
+//     },
+
+//     {
+//       sex: "m",
+
+//       age: 24,
+
+//       payment: 1883000,
+
+//       course: {
+//         title: "Graph",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 16,
+
+//       payment: 2190000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+
+//     {
+//       sex: "m",
+
+//       age: 19,
+
+//       payment: 718000,
+
+//       course: {
+//         title: "Graph",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 15,
+
+//       payment: 2195000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+
+//     {
+//       sex: "m",
+
+//       age: 24,
+
+//       payment: 460000,
+
+//       course: {
+//         title: "SMM",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 14,
+
+//       payment: 560000,
+
+//       course: {
+//         title: "SMM",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 22,
+
+//       payment: 160000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+
+//     {
+//       sex: "m",
+
+//       age: 26,
+
+//       payment: 883000,
+
+//       course: { title: "Graph" },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 26,
+
+//       payment: 750000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+
+//     {
+//       sex: "m",
+
+//       age: 23,
+
+//       payment: 212000,
+
+//       course: {
+//         title: "Graph",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 24,
+
+//       payment: 883000,
+
+//       course: {
+//         title: "Graph",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 27,
+
+//       payment: 190000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 19,
+
+//       payment: 210000,
+
+//       course: {
+//         title: "Graph",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 21,
+
+//       payment: 770000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+
+//     {
+//       sex: "m",
+
+//       age: 18,
+
+//       payment: 1200000,
+
+//       course: {
+//         title: "SMM",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 51,
+
+//       payment: 880000,
+
+//       course: {
+//         title: "SMM",
+//       },
+//     },
+
+//     {
+//       sex: "m",
+
+//       age: 51,
+
+//       payment: 2430000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 33,
+
+//       payment: 1277000,
+
+//       course: {
+//         title: "Graph",
+//       },
+//     },
+
+//     {
+//       sex: "w",
+
+//       age: 21,
+
+//       payment: 2750000,
+
+//       course: {
+//         title: "Dev",
+//       },
+//     },
+//   ];
+
+//   // В этом примере перед вами учебный центр, в котором учиться несколько студентов. Каждый из них учиться на каком-то курсе и платит за обучение. У студентов мы видим пол, возраст, оплату, а также курс, которые они выбрали.*
+
+//   // 1. Добавить студентам ключ howLong внутри ключа course, рядом с title. HowLong - цифра, означающая сколько месяцев длиться курс*
+
+//   // 2. Сохранить количество девочек и мальчиков внутри переменных m и w*
+
+//   // 3. Сохранить заработок учебного центра в earning.total*
+
+//   // 4. Узнать кто больше всех платит за обучение в earning.max*
+
+//   // 5. Узнать кто меньше всех платит за обучение в earning.min*
+
+//   // 6. Посчитать сколько студентов учиться на курсах внутри dev, smm и graph*
+
+//   // 7. Посчитать средний возраст студентов в average_age*
+
+//   // How to find amount of girls and boys students?
+//   // How to push new element into Objects?
+
+//   const setup = () => {
+//     // Task 1
+//     for (let i = 0; i < students.length; i++) {
+//       const addObject = new Object(students[i].course);
+//       addObject.howLong = 6;
+//     }
+
+//     // Task 2
+//     // Task 3
+//     // Task 4
+//     // Task 5
+
+//     // Task 6
+//     for (let i = 0; i < students.length; i++) {
+//       if (students[i].course === "Dev") {
+//         let sum1 = students[i].course;
+//         console.log(sum1);
+//       } else if (students[i].course.title === "Graph") {
+//         let sum2 = students[i].course;
+//         console.log(sum2);
+//       } else {
+//         console.log(students[i].course);
+//       }
+//     }
+//     // Task 7
+//     let average = students.reduce((a, b) => {
+//       return a + b.age / students.length;
+//     }, 0);
+//     console.log("Average age", Math.round(average));
+//   };
+//   setup();
